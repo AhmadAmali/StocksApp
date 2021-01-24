6fa99e32aa97bdf62cb30b497055bb9336c5569a
AhmadAbroad22 <ahmadamali77@gmail.com>
2021-01-24 22:25:25 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# StocksApp
+ a Flask web application to display real time stock data for a given list of companies using a financial data API
---
+++ main.py
@@ -0,0 +1,35 @@
+## main python file
+
+from pprint import pprint as pp
+from flask import Flask, flash, redirect, render_template, request, url_for
+from weather import query_api
+app = Flask(__name__)
+@app.route('/')
+
+def index():
+    return render_template(
+        'weather.html',
+        data=[{'name':'Tesla'}, {'name':'Apple'}, {'name':'Google'},
+        {'name':'Samsung'}, {'name':'Bitcoin'}, {'name':'Intuit'},
+        {'name':'Goldman Sachs'}, {'name':'Microsoft'}, {'name':''}, 
+        {'name':'Quebec'}])
+
+@app.route("/result" , methods=['GET', 'POST'])
+
+def result():
+    data = []
+    error = None
+    select = request.form.get('comp_select')
+    resp = query_api(select)
+    pp(resp)
+    if resp:
+       data.append(resp)
+    if len(data) != 2:
+        error = 'Bad Response from Stocks API'
+    return render_template(
+        'result.html',
+        data=data,
+        error=error)
+
+if __name__=='__main__':
+    app.run(debug=True)
